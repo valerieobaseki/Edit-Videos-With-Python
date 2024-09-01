3be1a23c86c75a02d5990d5efb7ba3c1cd03893e
valerieobaseki <34221811+valerieobaseki@users.noreply.github.com>
2024-09-01 09:24:22 -0500

    Update cutvideo.py
    Reformatted code

--- cutvideo.py
+++ cutvideo.py
@@ -1,12 +1,17 @@
 import os
-import shutil
-os.environ["IMAGEIO_FFMPEG_EXE"] = "/opt/homebrew/Cellar/ffmpeg/5.1.2_6/bin/ffmpeg"
-
 from moviepy.editor import VideoFileClip
-mclip = VideoFileClip("videoname.mp4")
-clip = VideoFileClip("videoname.mp4").subclip((13,00), (50,44))
 
-clip.write_videofile('videoname.mp4', codec='libx264', 
-                    audio_codec='aac', 
-                    temp_audiofile='temp-audio.m4a', 
-                    remove_temp=True)
+os.environ["IMAGEIO_FFMPEG_EXE"] = (
+    "/opt/homebrew/Cellar/ffmpeg/5.1.2_6/bin/ffmpeg"  # noqa: E501
+)
+mclip = VideoFileClip("videoname.mp4")
+clip = VideoFileClip("videoname.mp4").subclip((13, 00), (50, 44))
+
+clip.write_videofile(
+    "videoname.mp4",
+    codec="libx264",
+    audio_codec="aac",
+    temp_audiofile="temp-audio.m4a",
+    remove_temp=True,
+)
+
